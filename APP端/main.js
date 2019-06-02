f62ff04fb4ce33528a324821959e728997c49839
jkl33445 <34291670+jkl33445@users.noreply.github.com>
2019-06-02 20:43:07 +0800

    nengyuan-app
    app

---
+++ APP端/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+Vue.prototype.url = 'http://47.100.205.249:9000/'
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
